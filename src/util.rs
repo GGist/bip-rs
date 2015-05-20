f22f45edb05cc723571d4b847216a0632469942e
GGist <amiller4421@gmail.com>
2015-05-20 14:13:53 -0700

    Added test for util::fisher_shuffle()

--- src/util.rs
+++ src/util.rs
@@ -6,19 +6,6 @@ use std::hash::{Hash};
 
 use rand;
 
-/// Applies a Fisher-Yates shuffle on the given list.
-pub fn fisher_shuffle<T: Copy>(list: &mut [T]) {
-    for i in 0..list.len() {
-        let swap_index = (rand::random::<usize>() % (list.len() - i)) + i;
-        
-        let temp = list[i];
-        list[i] = list[swap_index];
-        list[swap_index] = temp;
-    }
-}
-
-//----------------------------------------------------------------------------//
-
 /// Trait for working with generic map data structures.
 pub trait Dictionary<K, V> where K: Borrow<str> {
     /// Convert the dictionary to an unordered list of key/value pairs.
@@ -56,5 +43,32 @@ impl<K, V> Dictionary<K, V> for BTreeMap<K, V> where K: Ord + Borrow<str> {
 
     fn insert(&mut self, key: K, value: V) -> Option<V> {
         self.insert(key, value)
+    }
+}
+
+/// Applies a Fisher-Yates shuffle on the given list.
+pub fn fisher_shuffle<T: Copy>(list: &mut [T]) {
+    for i in 0..list.len() {
+        let swap_index = (rand::random::<usize>() % (list.len() - i)) + i;
+        
+        let temp = list[i];
+        list[i] = list[swap_index];
+        list[swap_index] = temp;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn positive_fisher_shuffle() {
+        let mut test_slice = [1, 2, 3, 4];
+        
+        super::fisher_shuffle(&mut test_slice);
+        
+        assert!(test_slice.contains(&1));
+        assert!(test_slice.contains(&2));
+        assert!(test_slice.contains(&3));
+        assert!(test_slice.contains(&4));
     }
 }
