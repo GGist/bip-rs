e1507e382d1e171569e7f5c0ddf33732acd4e3ac
Andrew <amiller4421@gmail.com>
2015-12-17 21:21:57 -0800

    Update bip_dht example; Update Cargo.toml for all crates

--- bip_dht/examples/debug.rs
+++ bip_dht/examples/debug.rs
@@ -5,10 +5,10 @@ extern crate log;
 
 use std::collections::{HashSet};
 use std::io::{self, Read};
-use std::net::{ToSocketAddrs, SocketAddr, SocketAddrV4, Ipv4Addr};
+use std::net::{SocketAddr};
 use std::thread::{self};
 
-use bip_dht::{DhtBuilder, Router, MainlineDht};
+use bip_dht::{DhtBuilder, Router};
 use bip_handshake::{Handshaker};
 use bip_util::bt::{InfoHash, PeerId};
 
@@ -47,34 +47,29 @@ impl Handshaker for SimpleHandshaker {
     /// It is important that this is the external port that the peer will be sending data
     /// to. This is relevant if the client employs nat traversal via upnp or other means.
     fn port(&self) -> u16 {
-		6830
+		6889
 	}
 
     /// Initiates a handshake with the given socket address.
-    fn connect(&mut self, expected: Option<PeerId>, hash: InfoHash, addr: SocketAddr) {
-        let socket_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(104, 236, 141, 221), 6830));
-        if addr == socket_addr {
-            println!("FOUND OUR ADDRESS {:?}", addr);
-        }
-        
+    fn connect(&mut self, _: Option<PeerId>, _: InfoHash, addr: SocketAddr) {
         if self.filter.contains(&addr) {
             return
         }
         
         self.filter.insert(addr);
         self.count += 1;
-        println!("Received peer {:?}, total {}", addr, self.count);
+        println!("Received new peer {:?}, total unique peers {}", addr, self.count);
 	}
     
     /// Adds a filter that is applied to handshakes before they are initiated or completed.
-    fn filter<F>(&mut self, process: Box<F>) where F: Fn(SocketAddr) -> bool + Send {
+    fn filter<F>(&mut self, _: Box<F>) where F: Fn(SocketAddr) -> bool + Send {
 		()
 	}
     
     /// Stream that connections for the specified hash are sent to after they are successful.
     ///
     /// Connections MAY be dropped if all streams for a given hash are destroyed.
-    fn stream(&self, hash: InfoHash) -> () {
+    fn stream(&self, _: InfoHash) -> () {
 		()
 	}
 }
@@ -84,31 +79,28 @@ fn main() {
 		m.set(LogLevelFilter::max());
 		Box::new(SimpleLogger)
 	}).unwrap();
-	
-	let hash = InfoHash::from_bytes(b"BipChat: Room - Testing");
+	let hash = InfoHash::from_bytes(b"My Unique Info Hash");
     
-    let address = ("0.0.0.0", 6810).to_socket_addrs().unwrap().next().unwrap();
-    let dht = DhtBuilder::with_router(Router::Transmission).set_source_addr(address).set_read_only(false)
-    .start_mainline(SimpleHandshaker{ filter: HashSet::new(), count: 0 }).unwrap();
+    let handshaker = SimpleHandshaker{ filter: HashSet::new(), count: 0 };
+    let dht = DhtBuilder::with_router(Router::Transmission)
+    .set_read_only(false).start_mainline(handshaker).unwrap();
     
-    let stdin = io::stdin();
-    
+    // Spawn a thread to listen to and report events
     let events = dht.events();
     thread::spawn(move || {
         for event in events {
-            println!("RECEIVED DHT EVENT {:?}", event);
+            println!("\nReceived Dht Event {:?}", event);
         }
     });
     
-    let mut count = 0;
-    let mut announce = false;
-    for byte in stdin.bytes() {
-        if count == 0 {
-            count += 1;
-        } else {
-            dht.search(hash.into(), announce);
-            announce = !announce;
-            count = 0;
+    // Let the user announce or search on our info hash
+    let stdin = io::stdin();
+    let stdin_lock = stdin.lock();
+    for byte in stdin_lock.bytes() {
+        match &[byte.unwrap()] {
+            b"a" => dht.search(hash.into(), true),
+            b"s" => dht.search(hash.into(), false),
+            _   => ()
         }
     }
 }
