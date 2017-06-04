83bc41c13b5ff4596e6c7e62982e4c7ffdb79c71
GGist <amiller4421@gmail.com>
2017-06-04 16:26:25 -0700

    Add Benchmarks To bip_bencode

---
+++ bip_bencode/benches/mod.rs
@@ -0,0 +1,24 @@
+#![feature(test)]
+
+extern crate bip_bencode;
+extern crate test;
+
+#[cfg(test)]
+mod benches {
+    use bip_bencode::{BencodeRef, BDecodeOpt};
+    use test::Bencher;
+
+    #[bench]
+    fn bench_nested_lists(b: &mut Bencher) {
+        let bencode = b"lllllllllllllllllllllllllllllllllllllllllllllllllleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+
+        b.iter(|| BencodeRef::decode(&bencode[..], BDecodeOpt::new(50, true)).unwrap());
+    }
+
+    #[bench]
+    fn bench_multi_kb_bencode(b: &mut Bencher) {
+        let bencode = include_bytes!("multi_kb.bencode");
+
+        b.iter(|| BencodeRef::decode(&bencode[..], BDecodeOpt::default()).unwrap());
+    }
+}
