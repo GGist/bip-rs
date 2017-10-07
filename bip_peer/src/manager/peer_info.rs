575127cc862eeaaba90e5a4e361e8c125cd3ecb6
GGist <amiller4421@gmail.com>
2017-10-07 15:14:34 -0700

    Add Getters To PeerInfo

--- bip_peer/src/manager/peer_info.rs
+++ bip_peer/src/manager/peer_info.rs
@@ -15,4 +15,19 @@ impl PeerInfo {
     pub fn new(addr: SocketAddr, pid: PeerId, hash: InfoHash) -> PeerInfo {
         PeerInfo{ addr: addr, pid: pid, hash: hash }
     }
+
+    /// Retrieve the peer address.
+    pub fn addr(&self) -> &SocketAddr {
+        &self.addr
+    }
+
+    /// Retrieve the peer id.
+    pub fn peer_id(&self) -> &PeerId {
+        &self.pid
+    }
+    
+    /// Retrieve the peer info hash.
+    pub fn hash(&self) -> &InfoHash {
+        &self.hash
+    }
 }
