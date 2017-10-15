97a0e406dc85fa3b22fa7df6a635837d04c0adda
GGist <amiller4421@gmail.com>
2017-10-14 23:11:23 -0700

    Set Equality For PeerInfo To Exclude Extension Info

--- bip_peer/src/manager/peer_info.rs
+++ bip_peer/src/manager/peer_info.rs
@@ -1,10 +1,16 @@
+use std::hash::Hash;
+use std::hash::Hasher;
 use std::net::SocketAddr;
 
 use bip_handshake::Extensions;
 use bip_util::bt::{InfoHash, PeerId};
 
 /// Information that uniquely identifies a peer.
-#[derive(PartialEq, Eq, Hash, Debug, Copy, Clone)]
+/// 
+/// Equality oprations DO NOT INCLUDE `Extensions` as we define a
+/// unique peer as `(address, peer_id, hash)`, so equality will be
+/// based on that tuple.
+#[derive(Eq, Debug, Copy, Clone)]
 pub struct PeerInfo {
     addr: SocketAddr,
     pid:  PeerId,
@@ -36,5 +42,19 @@ impl PeerInfo {
     /// Retrieve the extensions supported by this peer.
     pub fn extensions(&self) -> &Extensions {
         &self.ext
+    }
+}
+
+impl PartialEq for PeerInfo {
+    fn eq(&self, other: &PeerInfo) -> bool {
+        self.addr.eq(&other.addr) && self.pid.eq(&other.pid) && self.hash.eq(&other.hash)
+    }
+}
+
+impl Hash for PeerInfo {
+    fn hash<H>(&self, state: &mut H) where H: Hasher {
+        self.addr.hash(state);
+        self.pid.hash(state);
+        self.hash.hash(state);
     }
 }
