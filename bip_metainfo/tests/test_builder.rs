73da6f25895cf4b17c09c2d308c3aad9f89c5a11
reeFridge <reefridgerator@gmail.com>
2018-02-03 17:39:54 +0700

    Added tests for builder's setters/getters

---
+++ bip_metainfo/tests/test_builder.rs
@@ -0,0 +1,40 @@
+extern crate bip_metainfo;
+
+use bip_metainfo::MetainfoBuilder;
+
+const TRACKER: &'static str = "udp://foo.bar.baz:6969";
+const DATE: i64 = 1517651523851;
+const COMMENT: &'static str = "Foo bar baz";
+const CREATED_BY: &'static str = "Fridge";
+
+#[test]
+fn positive_set_main_tracker() {
+    let builder = MetainfoBuilder::new()
+        .set_main_tracker(Some(TRACKER));
+
+    assert_eq!(builder.get_main_tracker(), Some(TRACKER.to_string()));
+}
+
+#[test]
+fn positive_set_creation_date() {
+    let builder = MetainfoBuilder::new()
+        .set_creation_date(Some(DATE));
+
+    assert_eq!(builder.get_creation_date(), Some(DATE));
+}
+
+#[test]
+fn positive_set_comment() {
+    let builder = MetainfoBuilder::new()
+        .set_comment(Some(COMMENT));
+
+    assert_eq!(builder.get_comment(), Some(COMMENT.to_string()));
+}
+
+#[test]
+fn positive_set_created_by() {
+    let builder = MetainfoBuilder::new()
+        .set_created_by(Some(CREATED_BY));
+
+    assert_eq!(builder.get_created_by(), Some(CREATED_BY.to_string()));
+}
